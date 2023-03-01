8f9770290b019aaa2d869f2abfa54f49ba0e2a83
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2023-03-01 15:23:51 +0100

    Remove obsolete inspection exclusion

--- src/extraction_benchmark/globals.py
+++ src/extraction_benchmark/globals.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-# noinspection PyUnresolvedReferences
 from extraction_benchmark.paths import *
 from extraction_benchmark.extractors import list_extractors
 
